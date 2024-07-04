ad9638f7ecd633b70000924b053111fc0bc20c51
Mags <98277311+seventeenmagpie@users.noreply.github.com>
2024-07-04 09:50:25 +0100

    Made working
    - sinope_mimic calls shimplugin every iteration,
    - shimplugin reads from shimsph.txt every time it is called
    - sinope_mimic should outpu the currents if the currents have changed.

--- sinope_mimic/shimplugin.cpp
+++ sinope_mimic/shimplugin.cpp
@@ -6,11 +6,12 @@
 
 #define NUM_CHANNELS 24
 float shimValues[NUM_CHANNELS];
-int i;
 
 EXTERN int ShimRealtime(float analogValue, float** output) {
-	// std::cout << "ShimRealtime was called." << std::endl;
-	
+	// custom code here
+
+	std::cout << "ShimRealtime was called." << std::endl;
+
 	std::string line;
 	std::ifstream current_file;
 	int this_current;
@@ -24,25 +25,14 @@ EXTERN int ShimRealtime(float analogValue, float** output) {
 
 	if (current_file.is_open())
 	{
-		std::cout << "Currents read from file: ";
-
+		std::cout << "shimsph.txt is open" << std::endl;
 		// reads the first 24 lines from shimsph.tmp
-		for ( i = 0; i != NUM_CHANNELS; ++i) {
+		for (int i = 0; i != NUM_CHANNELS; ++i) {
 			getline(current_file, line);
 			std::stringstream(line) >> this_current;
-			std::cout << this_current << " ";
-			shimValues[i] = ((float) this_current) / 1000;
+			shimValues[i] = ((float)this_current) / 1000;
 		}
-		std::cout << std::endl;
-
 		current_file.close();
-
-		std::cout << "Currents in shimValues: " << std::endl;
-		for (i = 0; i < NUM_CHANNELS; i++) {
-			std::cout << shimValues[i] << " ";
-		}
-		std::cout << std::endl;
-
 	}
 	else {
 		std::cout << "Unable to open file." << std::endl;
@@ -50,16 +40,5 @@ EXTERN int ShimRealtime(float analogValue, float** output) {
 
 	// do not change
 	*output = &(shimValues[0]);
-
-	// crashes here. don't know why.
-	std::cout << "output[1] is " << *(output[1]) << std::endl;
-
-	std::cout << "Checking contents of shimvalues vs output array: " << std::endl;
-	for (i = 0; i < NUM_CHANNELS; i++) {
-		std::cout << shimValues[i] << " vs ";
-		std::cout << *(output[i]) << std::endl;
-	}
-
-	std::cout << std::endl;
 	return NUM_CHANNELS;
 }
--- sinope_mimic/sinope_mimic.cpp
+++ sinope_mimic/sinope_mimic.cpp
@@ -14,7 +14,15 @@ int main()
     float* prev_currents;
     bool updated = false;
 
-    int number_of_channels = 0;
+    // this line is important because it pre-allocates prev_currents
+    // otherwise it crashes when we try and read prev_currents[1]
+    // which wouldn't exist.
+    // we can't allocate it normally because we don't know how many 
+    // channels there should be
+    int number_of_channels = ShimRealtime(analog, &prev_currents);
+    //if (prev_currents[4]) {
+    //    std::cout << "prev_currents was populated" << std::endl;
+    //}
     int i;
 
     while (analog <= 1000) {
@@ -22,18 +30,17 @@ int main()
         updated = false;
 
         number_of_channels = ShimRealtime(analog, &currents);
-
-        std::cout << "Currents as recieved by sinope_mimic: ";
+        //if (currents[4]) {
+        //    std::cout << "currents was populated" << std::endl;
+        //}
 
         for (i = 0; i < number_of_channels; i++) {
-            std::cout << currents[i] << " ";
-            if ((int) (currents[i]*1000) != (int) (prev_currents[i]*1000)) {
+            if ((int)(currents[i] * 1000) != (int)(prev_currents[i] * 1000)) {
                 prev_currents[i] = currents[i];
                 updated = true;
             }
-        }
 
-        std::cout << std::endl;
+        }
 
         if (updated) {
             std::cout << "New currents are: ";
@@ -41,9 +48,13 @@ int main()
                 std::cout << currents[i] << " ";
             }
             std::cout << std::endl;
-        } else {  // this loop goes very fast so this will spam.
-            std::cout << "Currents did not change." << std::endl;
-        }
+        } 
+        //else {  // this loop goes very fast so this will spam.
+        //    std::cout << "Currents did not change." << std::endl;
+        //}
+
+        std::cout << "Press enter." << std::endl;
+        std::cin.get();
     }
 
     return 0;
